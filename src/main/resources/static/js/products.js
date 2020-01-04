1f28f1391a1a0ee0a8360474ce100d32ba230d7f
Mikel <mbarcina001@gmail.com>
2020-01-04 14:37:20 +0100

    Added cart persistance

--- src/main/resources/static/js/products.js
+++ src/main/resources/static/js/products.js
@@ -1,3 +1,13 @@
+var productsInCart = JSON.parse(localStorage.getItem("productsInCart")) || [];
+
+class CartProduct {
+  constructor(id, price, quantity) {
+    this.id = id;
+    this.price = price;
+    this.quantity = quantity;
+  }
+}
+
 function loadProductsFromCart(){
 	for(var i=0; i<productsInCart.length; i++){
 		if(!$("#product"+productsInCart[i].id+" .add-to-cart").hasClass("hidden")){
@@ -6,6 +16,7 @@ function loadProductsFromCart(){
 		
 		if($("#product"+productsInCart[i].id+" .quantity").hasClass("hidden")){
 			$("#product"+productsInCart[i].id+" .quantity").removeClass("hidden")
+			$("#product"+productsInCart[i].id+" .quantity input").val(productsInCart[i].quantity)
 		}
 		
 		modifyProductList(productsInCart[i].quantity, productsInCart[i].id, productsInCart[i].price)
@@ -58,3 +69,44 @@ function onInputChange(element, id, price){
 	
 	modifyProductList(new_quantity, id, price)
 }	
+
+function modifyProductList(quantity, id, price){
+	
+	var found = false;
+	
+	for(var i=0; i<productsInCart.length; i++){
+		if(productsInCart[i].id === id){
+			found = true
+			
+			if(quantity>0){
+				productsInCart[i].quantity = quantity; 
+				productsInCart[i].price = price;
+			}else{
+				productsInCart.splice(i, 1)
+				break;
+			}
+		}
+	}
+	
+	if(!found){
+		productsInCart.push(new CartProduct(id, price, quantity));
+	}
+	
+	localStorage.setItem("productsInCart", JSON.stringify(productsInCart));
+}
+
+// TODO: REFACTOR
+function createDelivery(urlSuccess){				
+	$.ajax({ 
+		url: '/delivery/createDelivery',    
+	    type:"POST", 
+	    contentType: "application/json; charset=utf-8",
+	    data: JSON.stringify(productsInCart), //Stringified Json Object
+	    async: false,    //Cross-domain requests and dataType: "jsonp" requests do not support synchronous operation
+	    cache: false,    //This will force requested pages not to be cached by the browser          
+	    processData:false, //To avoid making query String instead of JSON
+	    success: function(){
+	        window.location.href = urlSuccess;
+    	}
+	});
+}
